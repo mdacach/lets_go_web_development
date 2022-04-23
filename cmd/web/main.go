e4d1588e5a12ad584de6008d0f23fb89d400ec38
Matheus Cardoso <dacachmatheus@gmail.com>
2022-04-22 23:13:56 -0300

    Add custom info and error loggers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,11 +4,21 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Parse the port address from command-line argument
+	// We can specify a default value, a description, and the variable will be
+	// automatically parsed to a string
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
+	flag.Parse() // Remember to parse the flags, otherwise you will always be using the default value
+
+	// We can create new loggers
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.Ldate|log.Ltime|log.LUTC)
+
+	// We can specify flags to choose what the logger will output
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)

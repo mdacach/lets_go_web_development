58e86d00b20e46a5f353d8f0310b3fd532b5be48
Matheus Cardoso <dacachmatheus@gmail.com>
2022-04-22 22:47:08 -0300

    Add static images through the site
    The book provided the images, css and javascript code.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,9 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	// Use the http.ListenAndServe() function to start a new web server. We pass
 	// two parameters: the TCP network address to listen on (in this case ":4000")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
